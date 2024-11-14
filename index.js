a958abcadc9e9d69c952be8574bfae6ba5689f59
Dean B <deantburrows88@gmail.com>
2024-11-14 09:29:00 +0100

    Update index.js
    refactored some if/else statements to ternary

--- index.js
+++ index.js
@@ -75,19 +75,10 @@ function getFeedHtml(){
     let feedHtml = ``
     
     tweetsData.forEach(function(tweet){
-        
-        let likeIconClass = ''
-        
-        if (tweet.isLiked){
-            likeIconClass = 'liked'
-        }
-        
-        let retweetIconClass = ''
-        
-        if (tweet.isRetweeted){
-            retweetIconClass = 'retweeted'
-        }
-        
+
+        const likeIconClass = tweet.isLiked ? 'liked' : ''
+        const retweetIconClass = tweet.isRetweeted ? 'retweeted' : ''
+                
         let repliesHtml = ''
         
         if(tweet.replies.length > 0){
